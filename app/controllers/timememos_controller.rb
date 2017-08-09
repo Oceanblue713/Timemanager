1a25c97a68c0fbc3540a87d80193ef369cf4dedd
Andy <amedamachip713@gmail.com>
2017-08-09 23:12:43 +0000

    Done Basic CRUD

--- app/controllers/timememos_controller.rb
+++ app/controllers/timememos_controller.rb
@@ -8,6 +8,44 @@ class TimememosController < ApplicationController
   end
   
   def new
-      @timememo = Timememo.new(content: '入力')
+      @timememo = Timememo.new
+  end
+  
+  def create
+      @timememo = Timememo.new(timememo_params)
+      
+    if @timememo.save
+    redirect_to @timememo
+    else
+    render :new
+    end
+  end
+  
+  def edit
+    @timememo = Timememo.find(params[:id])
+  end
+  
+  def update
+    @timememo = Timememo.find(params[:id])
+    
+    if @timememo.update(timememo_params)
+      redirect_to @timememo
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @timememo = Timememo.find(params[:id])
+    @timememo.destroy
+    
+    redirect_to timememos_url
+  end
+  
+  
+  private
+  
+  def timememo_params
+    params.require(:timememo).permit(:time, :place, :content)
   end
 end
